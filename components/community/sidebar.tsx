336ab06b5f60ca083ecc3b46ead0287c0fdecbd8
Davie <muigaid91@gmail.com>
2026-01-06 23:26:27 +0300

    made the bottom app bar have rounded top

--- components/community/sidebar.tsx
+++ components/community/sidebar.tsx
@@ -88,7 +88,7 @@ export default function Sidebar() {
       </aside>
 
       {/* Mobile Bottom App Bar */}
-      <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-50">
+      <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-50 rounded-t-xl">
         <div className="flex justify-around items-center h-16 px-2">
           {menuItems.map((item) => {
             const Icon = item.icon;
