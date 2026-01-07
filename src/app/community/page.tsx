2b42604af5c0ecccdfe62d07628b362cb87d0431
Davie <muigaid91@gmail.com>
2026-01-07 09:48:55 +0300

    changed the username text on posts  to black for visibility

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -359,7 +359,7 @@ export default function CommunityPage() {
                       </div>
                       <div>
                         <div className="flex items-center gap-2">
-                          <span className="font-semibold">{post.userName}</span>
+                          <span className="font-semibold text-black">{post.userName}</span>
                           {post.isVerified && (
                             <svg className="w-4 h-4 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
                               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" />
